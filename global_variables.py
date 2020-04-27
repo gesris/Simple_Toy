91a664b6025f51e80504bd7f43d0e7d62cefd22b
gesris <gessi.risto@gmx.de>
2020-04-27 12:18:38 +0200

    added global variables file

---
+++ global_variables.py
@@ -0,0 +1,12 @@
+# list of global variables as arrays 
+
+signal_x_coordinate = [-0.5]
+signal_y_coordinate = [0.5]
+background_x_coordinate = [0.5]
+background_y_coordinate = [-0.5]
+
+n_signal = [10000]
+n_background = [10000]
+
+def sign_value(array):
+    pass
